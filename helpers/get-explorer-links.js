9da729616662e25cf50deab2c4a5d91e870393f9
Victor Baranov <baranov.viktor.27@gmail.com>
2018-09-19 20:28:16 +0300

    Blockscout explorer links

--- helpers/get-explorer-links.js
+++ helpers/get-explorer-links.js
@@ -2,7 +2,7 @@ const getExplorerAccountLinkFor = (account, network) => {
     const prefix = getExplorerPrefix(network)
 	const POAnetwokIDs = [77, 99]
 	if (POAnetwokIDs.includes(parseInt(network))) {
-    	return `https://${prefix}poaexplorer.com/address/search/${account}`
+    	return `https://blockscout.com/poa/${prefix}/address/${account}`
     } else {
     	return `https://${prefix}etherscan.io/address/${account}`
     }
@@ -12,7 +12,7 @@ const getExplorerTxLinkFor = (hash, network) => {
 	const prefix = getExplorerPrefix(network)
 	const POAnetwokIDs = [77, 99]
      if (POAnetwokIDs.includes(parseInt(network))) {
-    	return `https://${prefix}poaexplorer.com/txid/search/${hash}`
+    	return `https://blockscout.com/poa/${prefix}/tx/${hash}`
     } else {
     	return `https://${prefix}etherscan.io/tx/${hash}`
     }
@@ -22,7 +22,7 @@ const getExplorerTokenLinkFor = (tokenAddress, account, network) => {
 	const prefix = getExplorerPrefix(network)
 	const POAnetwokIDs = [77, 99]
 	if (POAnetwokIDs.includes(parseInt(network))) {
-		return `https://${prefix}poaexplorer.com/address/search/${tokenAddress}`
+		return `https://blockscout.com/poa/${prefix}/address/${tokenAddress}`
 	} else {
 		return `https://${prefix}etherscan.io/token/${tokenAddress}?a=${account}`
 	}
@@ -33,26 +33,26 @@ function getExplorerPrefix (network) {
 	let prefix
 	switch (net) {
 	    case 1: // main net
-	    prefix = ''
-	    break
+		    prefix = ''
+		    break
 	    case 3: // ropsten test net
-	    prefix = 'ropsten.'
-	    break
+		    prefix = 'ropsten.'
+		    break
 	    case 4: // rinkeby test net
-	    prefix = 'rinkeby.'
-	    break
+		    prefix = 'rinkeby.'
+		    break
 	    case 42: // kovan test net
-	    prefix = 'kovan.'
-	    break
+		    prefix = 'kovan.'
+		    break
 	    case 77: // POA Sokol test net
-	    prefix = 'sokol.'
-	    break
+		    prefix = 'sokol'
+		    break
 	    case 99: // POA Core net
-	    console.warn('Do not use etherscan for POA Network')
-	    prefix = ''
-	    break
+		    console.warn('Do not use etherscan for POA Network')
+		    prefix = 'core'
+		    break
 	    default:
-	    prefix = ''
+	    	prefix = ''
 	}
 	return prefix
 }
